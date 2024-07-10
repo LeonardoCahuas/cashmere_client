2813a535c06473569e5cbf8c30b29e9a280ec154
Siva24Team <siva24official@gmail.com>
2024-07-10 11:45:38 +0200

    buttons

--- packages/ui/src/base/buttons.ts
+++ packages/ui/src/base/buttons.ts
@@ -2,10 +2,10 @@ import { Platform } from 'react-native'
 import { Button as BaseButton, styled } from 'tamagui'
 
 export const PrimaryButton = styled(BaseButton, {
-  width: 240,
+  width: "100%",
   height: 48,
   color: 'white',
-  backgroundColor: '#',
+  backgroundColor: '#1DAB61',
   borderRadius: 8,
   justifyContent: 'center',
   alignItems: 'center',
