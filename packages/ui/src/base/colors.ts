6c4c4999aed414a2aa583e2800d152f169cced3a
Rodrigo Weilg <contact@rodrigoweilg.com>
2024-07-16 18:51:03 -0500

    SIV-69: componente icone

--- packages/ui/src/base/colors.ts
+++ packages/ui/src/base/colors.ts
@@ -2,8 +2,8 @@ import type { ColorValue } from 'react-native'
 
 export const Colors: Record<string, ColorValue> = {
   greenPrimary: '#1DAB61',
-  blackPrimary:"#000000",
-  greyPrimary:"#737373",
-  greySecondary:"#DCDCDC",
-  bluePrimary:"#0296F8"
-} 
+  blackPrimary: '#000000',
+  greyPrimary: '#737373',
+  greySecondary: '#DCDCDC',
+  bluePrimary: '#0296F8',
+} as const
