f9511395a5cf92a14213970a9b99fca35d665af4
Rodrigo Weilg <contact@rodrigoweilg.com>
2024-08-12 17:12:27 -0500

    type tabs

---
+++ packages/ui/src/Tabs/typeTab.tsx
@@ -0,0 +1,49 @@
+import { Icon, IconName } from '@siva/ui'
+import React, { useState } from 'react'
+import { StyleSheet, View } from 'react-native'
+import { Colors } from '../base/colors'
+import CustomButton from './typeButton'
+
+const tabs: Array<TabItem> = [
+  { label: 'Macchine', icon: 'car' },
+  { label: 'Moto', icon: 'motorbike' },
+  { label: 'Furgoni', icon: 'truck' },
+]
+
+export const TypeTab: React.FC = () => {
+  const [activeTypes, setActiveTypes] = useState(tabs.map((tab) => ({ ...tab, selected: false })))
+
+  return (
+    <View style={styles.container}>
+      {activeTypes.map(({ label, icon, selected }, i) => (
+        <CustomButton
+          key={label}
+          icon={<Icon name={icon} color={selected ? 'white' : Colors.greySecondary} />}
+          label={label}
+          active={selected}
+          onClick={() => {
+            setActiveTypes((p) => {
+              const px = [...p]
+              px.splice(i, 1, { ...p[i], selected: !p[i].selected })
+              return px
+            })
+          }}
+        />
+      ))}
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    margin: 5,
+  },
+})
+
+interface TabItem {
+  label: string
+  icon: IconName
+}
--- packages/ui/src/base/colors.ts
+++ packages/ui/src/base/colors.ts
@@ -2,8 +2,8 @@ export const Colors = {
   greenPrimary: '#1DAB61',
   blackPrimary: '#000000',
   greyPrimary: '#737373',
-  greySecondary: '#DCDCDC',
+  greySecondary: '#6F7680',
   bluePrimary: '#0296F8',
   lightGray: '#F3F5F7',
-  tertiaryGray: '#F8F9F9'
+  tertiaryGray: '#F8F9F9',
 } as const
