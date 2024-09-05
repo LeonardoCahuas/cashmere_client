16c26447790e62bc62466b201a3479cb9b0c66bd
Rodrigo Weilg <contact@rodrigoweilg.com>
2024-09-05 18:19:16 -0500

    colors update

--- packages/ui/src/base/colors.ts
+++ packages/ui/src/base/colors.ts
@@ -2,7 +2,7 @@ export const Colors = {
   greenPrimary: '#00C15C',
   blackPrimary: '#000000',
   greyPrimary: '#737373',
-  greySecondary: '#6F7680',
+  greySecondary: '#DCDCDC',
   bluePrimary: '#0296F8',
   lightGray: '#F3F5F7',
   tertiaryGray: '#F8F9F9',
