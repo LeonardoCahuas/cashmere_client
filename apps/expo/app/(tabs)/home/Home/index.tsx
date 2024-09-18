7b2b49d968dfc2adcb77ebcf2b0c9b3a993078de
Rodrigo Weilg <contact@rodrigoweilg.com>
2024-09-18 17:24:27 -0500

    more reorg

--- apps/expo/app/(tabs)/home/Home/index.tsx
+++ apps/expo/app/(tabs)/home/Home/index.tsx
@@ -1,52 +1,14 @@
-import { AnimatedSwitch, Icon, PostingCard, TypeTab } from '@siva/ui'
+import { TypeTab } from '@siva/ui'
 import React from 'react'
 import { Image, ScrollView, StyleSheet, View } from 'react-native'
-import { ForYouSection } from './ForYouSection'
-import { HighlightedSection } from './HighligthedSection'
-import { SearchButton } from './components/SearchButton'
-import { SeeMoreButton } from './components/SeeMoreButton'
-import { SwitchSection } from './SwitchSection'
 import { BrandSection } from './BrandSection'
 import { DriverSection } from './DriverSection'
-
-type Posting = React.ComponentProps<typeof PostingCard.Large>['posting']
-type ImagesData = React.ComponentProps<typeof SeeMoreButton>['data']
-const tabs = [
-  {
-    label: 'Breve Termine',
-    icon: <Icon name="lightning" width={24} color="black" />,
-  },
-  {
-    label: 'Lungo Termine',
-    icon: <Icon name="clock" width={24} color="black" />,
-  },
-]
-
-const images: ImagesData = {
-  images: [
-    'https://mkvfjhboywoocbqdzilx.supabase.co/storage/v1/object/public/images/1.jpg?t=2024-09-06T04%3A03%3A19.187Z',
-    "https://mkvfjhboywoocbqdzilx.supabase.co/storage/v1/object/public/images/brands/yamaha.png",
-    "https://mkvfjhboywoocbqdzilx.supabase.co/storage/v1/object/public/images/brands/volkswagen.png?t=2024-09-12T08%3A36%3A24.014Z",
-  ],
-}
+import { ForYouSection } from './ForYouSection'
+import { HighlightedSection } from './HighligthedSection'
+import { SwitchSection } from './SwitchSection'
+import { SearchButton } from './components/SearchButton'
 
 const Home: React.FC = () => {
-  const posting: Posting = {
-    brand: 'Kia',
-    model: 'Seltos',
-    duration: 'MENSILE',
-    price: 550,
-    description: 'SUV Subcompatta',
-    imageUrl:
-      'https://mkvfjhboywoocbqdzilx.supabase.co/storage/v1/object/public/images/1.jpg?t=2024-09-06T04%3A03%3A19.187Z',
-    location: 'Milano',
-    owner: 'Fratelli Giacomel',
-    kmLimit: 500,
-    anticipo: 200,
-    minimumAge: 25,
-    minimumMonths: 36,
-  }
-
   return (
     <View style={styles.container}>
       <ScrollView contentContainerStyle={styles.scrollViewContainer}>
@@ -56,7 +18,7 @@ const Home: React.FC = () => {
             paddingHorizontal: 16,
             display: 'flex',
             alignItems: 'center',
-            gap:10
+            gap: 10,
           }}
         >
           <Image
@@ -71,10 +33,10 @@ const Home: React.FC = () => {
           <TypeTab />
         </View>
         <ForYouSection />
-        <SwitchSection/>
+        <SwitchSection />
         <HighlightedSection />
-        <DriverSection/>
-        <BrandSection/>
+        <DriverSection />
+        <BrandSection />
       </ScrollView>
     </View>
   )
@@ -89,6 +51,6 @@ const styles = StyleSheet.create({
   scrollViewContainer: {
     flexGrow: 1,
     paddingBottom: 96,
-    paddingTop:20
+    paddingTop: 20,
   },
 })
