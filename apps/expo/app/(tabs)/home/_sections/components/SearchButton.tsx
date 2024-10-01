c0ebbdafbcb0635a192d4f72a61eae0b3e9d98c9
Rodrigo Weilg <contact@rodrigoweilg.com>
2024-09-30 19:20:10 -0500

    navigation

--- apps/expo/app/(tabs)/home/_sections/components/SearchButton.tsx
+++ apps/expo/app/(tabs)/home/_sections/components/SearchButton.tsx
@@ -4,9 +4,7 @@ import { useRouter } from 'expo-router'
 export const SearchButton = () => {
   const router = useRouter()
 
-  const handlePress = () => {
-    router.push('/home/search')
-  }
+  const handlePress = () => router.push('/home/search')
 
   return (
     <PrimaryButton icon={<Icon name="search" color="white" />} onPress={handlePress}>
