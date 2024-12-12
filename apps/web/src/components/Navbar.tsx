4b9a73b267560e7a65626337f837db2be1e188fd
Rodrigo Weilg <contact@rodrigoweilg.com>
2024-12-11 23:10:02 -0500

    component fix

--- apps/web/src/components/Navbar.tsx
+++ apps/web/src/components/Navbar.tsx
@@ -1,12 +1,9 @@
-import Link from 'next/link'
-import { css } from '../../styled-system/css'
-import logoSiva from '../../public/siva_logo.svg'
-import heartIcon from '../../public/heart.svg'
-import chatIcon from '../../public/chat.svg'
-import rentIcon from '../../public/rent.svg'
-import userIcon from '../../public/user.svg'
-import burgerIcon from '../../public/burger.svg'
 import Image from 'next/image'
+import Link from 'next/link'
+import logoSiva from '../../public/siva_logo.svg'
+import { css } from '../../styled-system/css'
+import { Fill, PillButton, Variant } from './PillButton'
+import { HeartIcon } from './icons/Heart'
 
 export const NavBar = () => {
   const leftTabs = [
@@ -15,37 +12,12 @@ export const NavBar = () => {
     { label: 'Auto più noleggiate', url: '/trending' },
   ]
 
-  // Stile comune per i pulsanti
-  const buttonStyle = css({
-    display: 'flex',
-    alignItems: 'center',
-    gap: 2,
-    border: '1px solid',
-    borderColor:"dark-gray",
-    padding: '8px 12px',
-    borderRadius: '20px',
-    cursor: 'pointer',
-    fontSize: "0.8rem"
-  });
-  const greenButtonStyle = css({
-    display: 'flex',
-    alignItems: 'center', 
-    gap: 2,
-    padding: '8px 12px',
-    borderRadius: '20px',
-    cursor: 'pointer',
-    fontSize: "0.8rem",
-    backgroundColor: 'light-green',
-    color: 'primary-green',
-    border: 'none',
-  });
-
   return (
     <nav
       className={css({
         width: '100%',
         background: 'white',
-        height: '90px',
+        height: '84px',
         display: 'flex',
         alignItems: 'center',
         borderWidth: 1,
@@ -55,9 +27,9 @@ export const NavBar = () => {
         justifyContent: 'space-between',
       })}
     >
-      <div className={css({ display: 'flex', gap: 8, alignItems:"center" })}>
+      <div className={css({ display: 'flex', gap: 8, alignItems: 'center' })}>
         <div>
-          <Image src={logoSiva} alt="Logo Siva" width={60} height={50} /> 
+          <Image src={logoSiva} alt="Logo Siva" width={60} height={50} />
         </div>
         {leftTabs.map(({ label, url }) => (
           <Link key={url} href={url} className={css({ fontSize: 14, fontWeight: '500' })}>
@@ -66,22 +38,30 @@ export const NavBar = () => {
         ))}
       </div>
       <div className={css({ display: 'flex', gap: 8 })}>
-        <button className={buttonStyle}>
-          <Image src={heartIcon} alt="Preferiti" width={20} height={20} />
-          Preferiti
-        </button>
-        <button className={buttonStyle}>
-          <Image src={chatIcon} alt="Chat" width={20} height={20} />
-          Chat
-        </button>
-        <button className={greenButtonStyle}>
-          <Image src={rentIcon} alt="Inserisci annuncio" width={20} height={20} />
-          Inserisci annuncio
-        </button>
-        <button className={buttonStyle}>
-          <Image src={userIcon} alt="User" width={20} height={20} />
-          <Image src={burgerIcon} alt="Menu" width={20} height={20} />
-        </button>
+        <PillButton
+          label="Preferiti"
+          variant={Variant.neutral}
+          fill={Fill.outline}
+          icon={<HeartIcon />}
+        />
+        <PillButton
+          label="Chat"
+          variant={Variant.neutral}
+          fill={Fill.outline}
+          icon={<HeartIcon />}
+        />
+        <PillButton
+          label="Inserisci annuncio"
+          variant={Variant.success}
+          fill={Fill.none}
+          icon={<HeartIcon />}
+        />
+        <PillButton
+          label={<HeartIcon />}
+          variant={Variant.neutral}
+          fill={Fill.outline}
+          icon={<HeartIcon />}
+        />
       </div>
     </nav>
   )
